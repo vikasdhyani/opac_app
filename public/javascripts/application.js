91e274e8eaf21a00f73947d5563f9fac83d1063e
renuy <renu.yarday@strata.co.in>
2011-05-26 16:36:54 +0530

    removed network.jpg dynamicall select branch from db

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -302,35 +302,114 @@ $('#nm_reversal_cancel a').live('click', function(){
 var Sessionapp = {};
 Sessionapp.Charts = {};
 Sessionapp.ChartData = {};
+current_node_id="801_jb";
+function split_data(){
+  children=[];
+  jbClc={"id": IBTapp.ChartData["infovis"].id,
+        "name" : IBTapp.ChartData["infovis"].name,
+        "children":[],
+        "data":IBTapp.ChartData["infovis"].data}
+  
+  $.each(IBTapp.ChartData["infovis"].children, function(index, value) {
+      child={"id":value.id,
+             "name":value.name,
+             "data":value.data,
+             "children":[]}
+      children_1 = [];
+      children_1[0] = jbClc;
+      $.each(value.children, function(idx, val) {
+          child_1={"id": val.id,
+            "name" : val.name,
+            "children":val.children,//todo
+            "data":val.data}
+          children_1_1=[];
+            $.each(val.children,function(i,v){
+              child_1_1 = {"id": v.id,
+                    "name" : v.name,
+                    "children":[],
+                    "data":v.data}
+              children_1_1[i] = child_1_1;
+            });
+            IBTapp.ChartData[val.id]={
+              "id":val.id,
+              "name" : val.name,
+            "children":children_1_1,
+            "data":val.data};
+          children_1[idx+1]=child_1;
+      });
+      IBTapp.ChartData[value.id]={
+        "id":value.id,
+        "name" : value.name,
+      "children":children_1,
+      "data":value.data};
+      children[index] = child;
+    });
+    IBTapp.ChartData["801_jb"]={"id": IBTapp.ChartData["infovis"].id,
+      "name" : IBTapp.ChartData["infovis"].name,
+      "children":children,
+      "data":IBTapp.ChartData["infovis"].data}
+      
+}
 
 Sessionapp.showPanel = function () {
-w = 640;
-h= 640;
+w = 600;
+h= 600;
+split_data();
+
   var ht = new $jit.Hypertree({  
     //id of the visualization container  
     injectInto: 'infovis',  
     //canvas width and height  
     width: w,  
     height: h,  
+    levelsToShow : 2,
     //Change node and edge styles such as  
     //color, width and dimensions.  
     Node: {  
-        dim: 40,  
-        color: "#f00"  
+        //dim: 40,  
+        //color: "#f00",
+        overridable: true,
+          type: 'star',
+          color: '#ccb',
+          lineWidth: 1,
+          height: 5,
+          width: 5,
+          dim: 40,
+          transform: true    
     },  
     Edge: {  
         lineWidth: 2,  
-        color: "#088"  
-    },  
-    onBeforeCompute: function(node){  
+        color: "#088"  ,
+        overridable: false,
+          type: 'hyperline',
+        //  color: '#ccb',
+          //lineWidth: 1
+    },
+    duration: 700,
+    fps: 30,
+    //transition: Trans.Quart.easeInOut,
+    clearCanvas: true,
+    withLabels: true,  
+    onBeforePlotNode:function(node) {
         
+        if(node.selected) {  
+          //node.setData('color', '#f00');  
+        } else {  
+          //node.setData('color','#ccb');  
+        }  
+    },
+    onBeforeCompute: function(node){  
     },  
     //Attach event handlers and add text to the  
     //labels. This method is only triggered on label  
     //creation  
     onCreateLabel: function(domElement, node){  
         domElement.innerHTML = node.name;  
+        
+
         $jit.util.addEvent(domElement, 'click', function () {  
+          
+        
             ht.onClick(node.id, {  
                 onComplete: function() {  
                     ht.controller.onComplete();  
@@ -346,11 +425,10 @@ h= 640;
         style.cursor = 'pointer';  
         if (node._depth <= 1) {  
             style.fontSize = "1em";  
-            style.color = "#ddd";
+            style.color = "#1a1a1a";
         } else if(node._depth == 2){ 
-            style.display = 'none';  
-            //style.fontSize = "0.8em";  
-            //style.color = "#555";  
+            style.fontSize = "0.8em";  
+            style.color = "#555";  
     
         } else {  
             style.display = 'none';  
@@ -375,20 +453,30 @@ h= 640;
                 var rel = (child.data.band == node.name) ? child.data.relation : node.data.relation;  
                 html += "<li>" + child.name + " " + "<div class=\"relation\">(relation: " + rel + ")</div></li>";  
                 
-            }  
+            }
+            
         });  
         html += "</ul>";  
         //$jit.id('inner-details').innerHTML = html;  
+        
+        current_node_id = node.id;
         if (node.data.subdomain != 'city'){
-          alert(node.data.subdomain);
           url = '/auth/'+node.data.subdomain;
           //$(location).attr('href',url);
           document.location.replace(url);
-          }
+        }
+        else
+        {
+          ht.loadJSON(IBTapp.ChartData[current_node_id], 1);  
+          ht.refresh(); 
+        }
+          
     }  
-  });  
+  }
+  );  
   //load JSON data.  
-  ht.loadJSON(IBTapp.ChartData["infovis"]);  
+  
+  ht.loadJSON(IBTapp.ChartData[current_node_id], 1);  
   //compute positions and plot.  
   ht.refresh(); 
  }
