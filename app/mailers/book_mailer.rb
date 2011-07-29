29264de5c5bfe1bfda585c694c3a587ae9158b28
renuy <renu.yarday@strata.co.in>
2011-07-29 17:59:29 +0530

    showing isbn in mail

--- app/mailers/book_mailer.rb
+++ app/mailers/book_mailer.rb
@@ -2,8 +2,9 @@ class BookMailer < ActionMailer::Base
   default :from => "mc@strataretail.co"
   
   
-  def isbn_not_found(book, user)
+  def isbn_not_found(book, user, isbn)
     @book = book
+    @isbn = isbn
     @user = user
     to_ids = 'tech.support@justbooksclc.com'
     
