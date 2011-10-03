4af26da0673e1cf6f1b60933c466031a53b7b67f
Tejas Dinkar <tejas@gja.in>
2011-10-03 20:09:42 +0530

    Showing the delivery type

---
+++ app/helpers/order_list_helper.rb
@@ -0,0 +1,5 @@
+module OrderListHelper
+  def delivery_type(delivery_order)
+    delivery_order.pickup? ? "Pickup" : "Delivery"
+  end
+end
---
+++ spec/helpers/order_list_helper_spec.rb
@@ -0,0 +1,14 @@
+require "spec_helper"
+
+describe OrderListHelper do
+
+  it "should decode delivery type" do
+    delivery_order = Factory(:delivery_order, :order_type => DeliveryOrder::DELIVERY)
+    helper.delivery_type(delivery_order).should == 'Delivery'
+  end
+
+  it "should decode pickup type" do
+    delivery_order = Factory(:delivery_order, :order_type => DeliveryOrder::PICKUP)
+    helper.delivery_type(delivery_order).should == 'Pickup'
+  end
+end
