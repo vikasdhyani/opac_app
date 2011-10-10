27b2e167273193a0fdc771220d8cd338e1246894
Tejas Dinkar <tejas@gja.in>
2011-10-10 11:42:28 +0530

    models for delivery schedule and delivery slots added.

--- app/models/delivery_schedule.rb
+++ app/models/delivery_schedule.rb
@@ -1,3 +1,6 @@
 class DeliverySchedule < ActiveRecord::Base
   belongs_to :delivery_slot
+  validates_presence_of :delivery_date
+  validates_presence_of :delivery_slot_id
+  validates_uniqueness_of :delivery_date, :scope => :delivery_slot_id
 end
--- spec/models/delivery_schedule_spec.rb
+++ spec/models/delivery_schedule_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe DeliverySchedule do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+  subject { Factory(:delivery_schedule) }
+  it { should belong_to(:delivery_slot) }
+  it { should validate_presence_of(:delivery_slot_id) }
+  it { should validate_presence_of(:delivery_date) }
+  it { should validate_uniqueness_of(:delivery_date).scoped_to(:delivery_slot_id)}
+  end
 end
