ab7ccd25349f62e0cce32e7cc37c46082f3ada12
Tejas Dinkar <tejas@gja.in>
2011-10-10 11:06:40 +0530

    Added a delivery slot

---
+++ app/models/delivery_slot.rb
@@ -0,0 +1,2 @@
+class DeliverySlot < ActiveRecord::Base
+end
--- spec/models/factories_spec.rb
+++ spec/models/factories_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Factories" do
-  [:delivery_order, :ibtr, :user, :dev_membership].each do |type|
+  [:delivery_order, :ibtr, :user, :dev_membership, :delivery_slot].each do |type|
     it "can create a valid object of #{type}" do
       Factory.build(type).should be_valid
     end
