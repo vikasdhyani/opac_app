c2d682cb23a9331e0dae8de80cede2ae35a2ff21
Tejas Dinkar <tejas@gja.in>
2011-10-11 17:00:12 +0530

    Merged two javascript classes

--- spec/javascripts/delivery_order_spec.js
+++ spec/javascripts/delivery_order_spec.js
@@ -1,8 +1,9 @@
 describe("DeliveryOrder", function() {
+  var deliveryOrder = new Strata.DeliveryOrder($(".order_lists"), "/foobar", "/submit/schedule");
+
   describe("when no notes are visible", function() {
     beforeEach(function() {
       loadFixtures("delivery_order_with_no_notes_visible.html");
-      new Strata.DeliveryOrder($(".order_lists"))
     });
 
     it("fetch notes when notes button is clicked", function() {
@@ -34,7 +35,6 @@ describe("DeliveryOrder", function() {
   describe("when notes are visible", function() {
     beforeEach(function() {
       loadFixtures("delivery_order_with_one_note_visible.html");
-      new Strata.DeliveryOrder($(".order_lists"))
     });
 
     it("hides the member's notes on clicking the hide link", function() {
@@ -71,7 +71,7 @@ describe("DeliveryOrder", function() {
       });
 
       it("disables the add notes button after adding a new comment", function() {
-        spyOn($, "ajax")
+        spyOn($, "ajax");
         $(".notesTextArea").text("foo");
         $(".add_notes_button").click();
         expect($(".add_notes_button")).toBeDisabled();
@@ -91,6 +91,115 @@ describe("DeliveryOrder", function() {
         $(".notesTextArea").text("foo");
         $(".add_notes_button").click();
         expect($(".add_notes_button")).not.toBeDisabled();
+      });
+    });
+  });
+
+  describe("when schedule is hidden", function() {
+    beforeEach(function() {
+      loadFixtures("delivery_order_with_schedule_hidden.html");
+    });
+
+    it("makes an ajax call to get new form", function() {
+      spyOn($, "ajax").andCallFake(function(params) {
+        expect(params.url).toEqual("/foobar");
+      });
+      $(".scheduleDeliveryButton").click();
+      expect($.ajax).wasCalled();
+    });
+
+    it("replaces the existing div", function() {
+      spyOn($, "ajax").andCallFake(function(params) {
+        params.success('<input class="datePicker"/>')
+      });
+      $(".scheduleDeliveryButton").click();
+      expect($(".scheduleDelivery .datePicker")).toExist();
+      expect($(".datePicker")).toHaveClass("hasDatepicker");
+    });
+  });
+
+  describe("when schedule is visible", function() {
+    beforeEach(function() {
+      loadFixtures("delivery_order_with_schedule_visible.html");
+    });
+
+    it("should hide new appointment form on clicking cancel", function() {
+      $(".cancelSchedule").click();
+      expect($(".scheduleDelivery .scheduleDeliveryButton")).toExist();
+    });
+
+    describe("submitting new form", function() {
+      var tomorrow;
+      var dateFormat = "dd/M/yy";
+
+      beforeEach(function() {
+        $("#checkboxToBeSelected").attr("checked", true);
+        $("select").val("1000");
+        tomorrow = new Date();
+        tomorrow.setTime(tomorrow.getTime() + (1000 * 3600 * 24));
+        $(".datePicker").val($.datepicker.formatDate(dateFormat, tomorrow));
+      });
+
+      it("on submitting new form, selected delivery orders should have chosen delivery schedule", function() {
+        spyOn($, "ajax").andCallFake(function(params) {
+          expect(params.url).toEqual("/submit/schedule");
+        });
+        $(".submitButton").click();
+        expect($.ajax).wasCalled();
+      });
+
+      it("passes a list of delivery_order ids, date and delivery_slot id", function() {
+        spyOn($, "ajax").andCallFake(function(params) {
+          expect(params.data.delivery_date).toEqual($.datepicker.formatDate(dateFormat, tomorrow));
+          expect(params.data.delivery_slot_id).toEqual("1000");
+          expect(params.data.delivery_orders).toEqual(["1234"]);
+        });
+        $(".submitButton").click();
+        expect($.ajax).wasCalled();
+      });
+
+      it("show the data returned by server on successfully submitting", function() {
+        spyOn($, "ajax").andCallFake(function(params) {
+          params.success("foobar")
+        });
+        $(".submitButton").click();
+        expect($(".scheduleDelivery")).toHaveText("foobar");
+      });
+
+      describe("validations", function() {
+        it("should not fix the delivery schedule if no checkbox is selected", function() {
+          spyOn($, "ajax");
+          $("#checkboxToBeSelected").attr("checked", false);
+          $(".submitButton").click();
+          expect($.ajax).not.wasCalled();
+          expect($(".scheduleErrorMessages")).toHaveText("Please select at least one order to schedule");
+        });
+
+        it("should not fix the delivery schedule if date is not selected", function() {
+          spyOn($, "ajax");
+          $(".datePicker").val("");
+          $(".submitButton").click();
+          expect($.ajax).not.wasCalled();
+          expect($(".scheduleErrorMessages")).toHaveText("Please select a date to schedule");
+        });
+
+        it("should not fix the delivery schedule if delivery slot is not selected", function() {
+          spyOn($, "ajax");
+          $("select").val("");
+          $(".submitButton").click();
+          expect($.ajax).not.wasCalled();
+          expect($(".scheduleErrorMessages")).toHaveText("Please select a delivery slot to schedule");
+        });
+
+        it("should not fix the delivery schedule if delivery slot is not selected", function() {
+          var yesterday = new Date();
+          yesterday.setTime(yesterday.getTime() - (1000 * 3600 * 24));
+          spyOn($, "ajax");
+          $(".datePicker").val($.datepicker.formatDate(dateFormat, yesterday));
+          $(".submitButton").click();
+          expect($.ajax).not.wasCalled();
+          expect($(".scheduleErrorMessages")).toHaveText("Please select a delivery date in future");
+        });
       });
     });
   });
