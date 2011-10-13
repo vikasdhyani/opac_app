86f54651e66013fcf31bace91383b5677d23024d
Tejas Dinkar <tejas@gja.in>
2011-10-13 12:15:41 +0530

    Deleted a view

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,7 +4,7 @@ class AppointmentsController < ApplicationController
     schedule = DeliverySchedule.find_or_create_by_delivery_date_and_delivery_slot_id(delivery_date, params[:delivery_slot_id])
     schedule.add_delivery_orders_by_id params[:delivery_orders]
     if schedule.save
-      render :layout => false
+      render :text => "Selected deliveries scheduled successfully"
     else
       render :json => { :errors => schedule.errors.full_messages }, :status => :unprocessable_entity
     end
